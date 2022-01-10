e8a4fb579bfa1c2d99aacc588bb43066573fc9e1
umeiko <58870893+umeiko@users.noreply.github.com>
2022-01-11 01:21:00 +0800

    Add files via upload

---
+++ StepMotor.h
@@ -0,0 +1,98 @@
+#ifndef StepMotor_H //预编译指令，防止重复定义类
+#define StepMotor_H
+
+class StepMotor{
+    public:
+        StepMotor(uint8_t, uint8_t, uint8_t); //类的构造函数，与类名相同
+        void motorSpeed(double);
+        void strokeInit(uint8_t, uint8_t, uint8_t);
+        void loop_strokeChecker();
+        uint8_t dirState;
+        double  nowFreq;
+        int32_t nowDistance=0;
+    private:
+        uint8_t motorChannel;
+        uint8_t dirPin;
+        uint8_t pulPin;
+        uint8_t positivePin;
+        uint8_t negativePin;
+        uint8_t strokeOnPinMode  = HIGH;  // 限位开关为“触发”时这个管脚的状态
+        uint8_t positivePinValue = 0;
+        uint8_t negativePinValue = 0;
+};
+
+StepMotor :: StepMotor(uint8_t Pul, uint8_t Dir, uint8_t motorChannel){
+    // 初始化一个电机，提供其关联管脚与电机号
+    this->motorChannel = 2*motorChannel;
+    this->dirPin = Dir;
+    this->pulPin = Pul;
+    pinMode(dirPin, OUTPUT);
+    digitalWrite(dirPin, LOW);
+    ledcSetup(motorChannel, 0, 10);
+    ledcAttachPin(Pul, motorChannel);
+}
+
+void StepMotor :: motorSpeed(double freq_Speed){
+    // 设置电机的速度
+    // 只有没有触发限位时才有效
+    if(freq_Speed>0 && positivePin!=strokeOnPinMode){
+        digitalWrite(dirPin, HIGH);
+        this->dirState = HIGH;
+    }
+    else if (freq_Speed<0 && negativePin!=strokeOnPinMode){
+        digitalWrite(dirPin, LOW);
+        this->dirState = LOW;
+    }
+    this->nowFreq = abs(freq_Speed);
+    ledcWriteTone(motorChannel, nowFreq);
+}
+
+void StepMotor :: strokeInit(uint8_t positivePin, uint8_t negativePin, uint8_t strokeOnPinMode=HIGH){
+    // 初始化限位开关
+    pinMode(positivePin, OUTPUT);
+    pinMode(negativePin, OUTPUT);
+    this->positivePinValue = digitalRead(positivePin);
+    this->negativePinValue = digitalRead(negativePin);
+    this->positivePin = positivePin;
+    this->negativePin = negativePin;
+    this->strokeOnPinMode = strokeOnPinMode;
+}
+
+void StepMotor :: loop_strokeChecker(){
+    // 把这个放在主循环里来做限位检查
+    uint8_t now_positive = !strokeOnPinMode;
+    uint8_t now_negative = !strokeOnPinMode;
+    // 信号滤波
+    if(digitalRead(positivePin) == strokeOnPinMode){
+        delay(1);
+        if(digitalRead(positivePin)==strokeOnPinMode){
+            now_positive = strokeOnPinMode;
+        }
+    }
+    if(digitalRead(negativePin) == strokeOnPinMode){
+        delay(1);
+        if(digitalRead(negativePin)==strokeOnPinMode){
+            now_negative = strokeOnPinMode;
+        }
+    }
+
+    if(positivePinValue!=strokeOnPinMode && now_positive==strokeOnPinMode){
+        // 开启沿触发
+        if(nowFreq!=0.0 && dirState){
+            ledcWriteTone(motorChannel, 0.0);
+            this->nowFreq = 0.0;
+        }
+    }
+
+    if(negativePinValue!=strokeOnPinMode && now_negative==strokeOnPinMode){
+        // 开启沿触发
+        if(nowFreq!=0.0 && !dirState){
+            ledcWriteTone(motorChannel, 0.0);
+            this->nowFreq = 0.0;
+        }
+    }
+    // 更新限位状态
+    this->positivePinValue = now_positive;
+    this->positivePinValue = now_negative;
+}
+#endif
